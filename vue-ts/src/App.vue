d9dc466aa04a265667a4c12bf4f6822ff815b001
jyKang2 <jy.kang@ilts.co.kr>
2019-11-25 13:38:42 +0900

    vue-ts 기반 프로젝트 셋업

---
+++ vue-ts/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    <Header />
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view />
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+import Header from '@/components/Header.vue';
+
+@Component({
+  components: {
+    Header
+  }
+})
+export default class HelloWorld extends Vue {
+  @Prop() private msg!: string;
+}
+</script>
+
+<style lang="scss">
+body {
+  margin: 0;
+  padding: 0;
+}
+
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+}
+</style>
