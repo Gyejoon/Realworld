ef482b23bef2aa3767e248d474780df39498077b
Jun Young Kang <00700mm@gmail.com>
2019-12-02 22:43:23 +0900

    Banner 문구 수정

--- vue-ts/src/components/TheBanner.vue
+++ vue-ts/src/components/TheBanner.vue
@@ -2,7 +2,7 @@
   <div class="banner">
     <div class="container">
       <h1 class="logo-font">conduit</h1>
-      <p>당신의 지식을 공유해보세요.</p>
+      <p>지식을 공유해보세요.</p>
     </div>
   </div>
 </template>
