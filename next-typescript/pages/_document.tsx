0b4a31fe616c53aa7d15754cb10be69d82afdf6f
Jun Young Kang <00700mm@gmail.com>
2020-08-03 21:44:05 +0900

    chore: favicon 추가

--- next-typescript/pages/_document.tsx
+++ next-typescript/pages/_document.tsx
@@ -40,6 +40,17 @@ export default class MyDocument extends Document {
           <meta charSet="utf-8" />
           <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
         </Head>
+        <link
+          href="//code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css"
+          rel="stylesheet"
+          type="text/css"
+        />
+        <link
+          href="//fonts.googleapis.com/css?family=Titillium+Web:700|Source+Serif+Pro:400,700|Merriweather+Sans:400,700|Source+Sans+Pro:400,300,600,700,300italic,400italic,600italic,700italic"
+          rel="stylesheet"
+          type="text/css"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
         <link rel="stylesheet" href="//demo.productionready.io/main.css" />
         <body>
           <Main />
